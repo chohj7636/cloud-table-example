b9b48295333b4d1153327980a5f15829c97064df
HyunJunCHO <chohj7636@gmail.com>
2025-09-23 22:37:19 +0900

    Collapsible 한 효과를 주고, 동작할때 간단한 애니메이션과 함께 동작할 수 있도록 적용

--- src/features/dialog/ui/DialogDetailConfig.tsx
+++ src/features/dialog/ui/DialogDetailConfig.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/shared/components/ui/collapsible';
 import { Input } from '@/shared/components/ui/input';
 import { Label } from '@/shared/components/ui/label';
 import { Switch } from '@/shared/components/ui/switch';
@@ -10,6 +15,7 @@ import {
   GCPEventSource,
   Provider,
 } from '@/shared/types/types';
+import { ChevronDownIcon } from 'lucide-react';
 
 interface DialogDetailConfigProps {
   provider: Provider;
@@ -37,6 +43,27 @@ const DialogDetailConfig: React.FC<DialogDetailConfigProps> = React.memo(
     setEventProcessEnabled,
     setUserActivityEnabled,
   }) => {
+    // state
+    const [isOpenCollapsible, setIsOpenCollapsible] = useState(false);
+
+    // ref
+    const collapsibleRef = useRef<HTMLDivElement>(null);
+    const contentBottomRef = useRef<HTMLDivElement>(null);
+
+    // 펼쳐질 때 스크롤 자동 이동
+    useEffect(() => {
+      if (isOpenCollapsible && contentBottomRef.current) {
+        // 애니메이션 완료 후 바닥으로 스크롤 이동
+        setTimeout(() => {
+          contentBottomRef.current?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end',
+            inline: 'nearest',
+          });
+        }, 300); // 애니메이션 시간(300ms) 후 실행
+      }
+    }, [isOpenCollapsible]);
+
     const handleCloudTrailNameChange = (
       e: React.ChangeEvent<HTMLInputElement>,
     ) => {
@@ -111,67 +138,84 @@ const DialogDetailConfig: React.FC<DialogDetailConfigProps> = React.memo(
     };
 
     return (
-      <div className="space-y-8">
-        <h3 className="border-b py-2 pl-2 text-lg font-bold">고급 설정</h3>
-        <div className="space-y-6 pl-4">
-          {/* Event Integration */}
-          {provider && (
-            <div className="flex flex-col gap-4">
-              <Label className="text-[16px]">Event Integration</Label>
-              {printEventSourceComponent(provider)}
-            </div>
-          )}
-
-          {/* Cloud Group Name */}
-          <div className="flex flex-col gap-2">
-            <Label className="text-[16px]">Cloud Group</Label>
-            <div className="grid grid-cols-2 gap-2 rounded-md border p-3">
-              {CloudGroupNameList.map((group) => (
-                <label
-                  key={group}
-                  className="flex cursor-pointer items-center gap-2"
-                >
-                  <input
-                    type="checkbox"
-                    checked={cloudGroupName?.includes(group) || false}
-                    onChange={() => handleCloudGroupToggle(group)}
-                    className="h-4 w-4"
-                  />
-                  <span className="text-sm capitalize">{group}</span>
-                </label>
-              ))}
-            </div>
+      <Collapsible open={isOpenCollapsible} onOpenChange={setIsOpenCollapsible}>
+        <div ref={collapsibleRef} className="mb-4 space-y-8">
+          {/* Collapsible한 컴포넌트로 구현 */}
+          <div className="flex w-full items-center gap-2 border-b">
+            <h3 className="py-2 pl-2 text-lg font-bold">고급 설정</h3>
+            <CollapsibleTrigger asChild>
+              <ChevronDownIcon
+                className={`size-5 transition-transform duration-300 ${
+                  isOpenCollapsible ? 'rotate-180' : ''
+                }`}
+                onClick={() => setIsOpenCollapsible(!isOpenCollapsible)}
+              />
+            </CollapsibleTrigger>
           </div>
+          <CollapsibleContent>
+            <div className="space-y-6 pl-4">
+              {/* Event Integration */}
+              {provider && (
+                <div className="flex flex-col gap-4">
+                  <Label className="text-[16px]">Event Integration</Label>
+                  {printEventSourceComponent(provider)}
+                </div>
+              )}
 
-          <div className="h-px w-full bg-gray-200" />
+              {/* Cloud Group Name */}
+              <div className="flex flex-col gap-2">
+                <Label className="text-[16px]">Cloud Group</Label>
+                <div className="grid grid-cols-2 gap-2 rounded-md border p-3">
+                  {CloudGroupNameList.map((group) => (
+                    <label
+                      key={group}
+                      className="flex cursor-pointer items-center gap-2"
+                    >
+                      <input
+                        type="checkbox"
+                        checked={cloudGroupName?.includes(group) || false}
+                        onChange={() => handleCloudGroupToggle(group)}
+                        className="h-4 w-4"
+                      />
+                      <span className="text-sm capitalize">{group}</span>
+                    </label>
+                  ))}
+                </div>
+              </div>
 
-          {/* Toggle Settings */}
-          <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-            <div className="space-y-0.5">
-              <Label className="text-[16px]">Enable Event Process</Label>
-              <p className="text-[14px] text-gray-500">
-                Whether to process real-time events from the cloud.
-              </p>
+              <div className="h-px w-full bg-gray-200" />
+
+              {/* Toggle Settings */}
+              <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                <div className="space-y-0.5">
+                  <Label className="text-[16px]">Enable Event Process</Label>
+                  <p className="text-[14px] text-gray-500">
+                    Whether to process real-time events from the cloud.
+                  </p>
+                </div>
+                <Switch
+                  checked={eventProcessEnabled}
+                  onCheckedChange={handleEventProcessToggle}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                <div className="space-y-0.5">
+                  <Label className="text-[16px]">Enable User Activity</Label>
+                  <p className="text-[14px] text-gray-500">
+                    Track the usage patterns of users in the cloud.
+                  </p>
+                </div>
+                <Switch
+                  checked={userActivityEnabled}
+                  onCheckedChange={handleUserActivityToggle}
+                />
+              </div>
+              {/* 스크롤 바닥 기준점 */}
+              <div ref={contentBottomRef} />
             </div>
-            <Switch
-              checked={eventProcessEnabled}
-              onCheckedChange={handleEventProcessToggle}
-            />
-          </div>
-          <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-            <div className="space-y-0.5">
-              <Label className="text-[16px]">Enable User Activity</Label>
-              <p className="text-[14px] text-gray-500">
-                Track the usage patterns of users in the cloud.
-              </p>
-            </div>
-            <Switch
-              checked={userActivityEnabled}
-              onCheckedChange={handleUserActivityToggle}
-            />
-          </div>
+          </CollapsibleContent>
         </div>
-      </div>
+      </Collapsible>
     );
   },
 );
